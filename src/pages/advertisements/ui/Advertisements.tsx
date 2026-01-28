c721d349ac31f995ee206254a7082eef634af18a
slec16 <sankopytov.76@mail.ru>
2026-01-28 03:37:04 +0300

    feat: advertisements filter template

--- src/pages/advertisements/ui/Advertisements.tsx
+++ src/pages/advertisements/ui/Advertisements.tsx
@@ -1,10 +1,53 @@
-
-
+import { Button, Select } from 'antd'
+import styles from './Advertisements.module.scss'
+import Search from '@/features/advertisement-search'
+import { SortAscendingOutlined } from '@ant-design/icons'
 
 const Advertisements = () => {
 
-    return(
-        <div>advertisements</div>
+    const handleChange = (value: string) => {
+        console.log(`selected ${value}`)
+    }
+
+    return (
+        <div >
+            <div className={styles.header}>
+                <h2 className={styles.title}>Список товаров</h2>
+                <Button type='primary'>Добавить товар</Button>
+            </div>
+            <div className={styles.filtersContainer}>
+                <div className={styles.search}>
+                    <Search />
+                </div>
+                <Select
+                    size='large'
+                    placeholder="Фильтры"
+                    style={{ width: 200, marginRight: 10 }}
+                    allowClear
+                    onChange={handleChange}
+                    options={[
+                        { value: 'price', label: 'Цена' },
+                        { value: 'views', label: 'Просмотры' },
+                        { value: 'likes', label: 'Лайки' },
+                    ]}
+                />
+                <Select
+                    size='large'
+                    placeholder="Сортировка"
+                    prefix={<SortAscendingOutlined />}
+                    style={{ width: 200 }}
+                    allowClear
+                    onChange={handleChange}
+                    options={[
+                        { value: 'name', label: 'Имя' },
+                        { value: 'price', label: 'Цена' },
+                        { value: 'createdAt', label: 'Дата создания' },
+                        { value: 'views', label: 'Просмотры' },
+                        { value: 'likes', label: 'Лайки' },
+                    ]}
+                />
+            </div>
+        </div>
     )
 }
 
